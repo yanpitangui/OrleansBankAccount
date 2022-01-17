e696181adb1bf832c2c447ed1db3b0634475566a
Yan Pitangui <yanpitangui@gmail.com>
2022-01-17 18:55:39 -0300

    Added some validation and added notification to transfer failed/success

--- Frontend/Services/BankService.cs
+++ Frontend/Services/BankService.cs
@@ -16,4 +16,9 @@ public class BankService : IBankService
     }
 
     public Task<IEnumerable<AccountViewModel>?> ListAccounts() => _client.GetFromJsonAsync<IEnumerable<AccountViewModel>>("/api/account");
+    public Task<HttpResponseMessage> TransferAmount(Guid? modelAccountFrom, Guid? modelAccountTo, decimal modelAmount)
+    {
+        return _client
+            .PostAsJsonAsync($"/api/account/transfer/{modelAccountFrom}/{modelAccountTo}", modelAmount);
+    }
 }
--- Frontend/Services/IBankService.cs
+++ Frontend/Services/IBankService.cs
@@ -4,4 +4,5 @@ public interface IBankService
 {
     Task CreateAccount(string userName, decimal balance);
     Task<IEnumerable<AccountViewModel>?> ListAccounts();
+    Task<HttpResponseMessage> TransferAmount(Guid? modelAccountFrom, Guid? modelAccountTo, decimal modelAmount);
 }
